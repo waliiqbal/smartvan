90e67c74fca53dc0a47b4c8d2ec9d52814f2854f
waliiqbal <waliiqbal2020@gmail.com>
2026-03-14 06:16:01 -0700

    hyhu

--- src/alert/alert.module.ts
+++ src/alert/alert.module.ts
@@ -3,11 +3,13 @@ import { Module } from '@nestjs/common';
 
 import { AlertController } from './alert.controller';
 import { alertService } from './alert.service';
+import { FirebaseAdminModule } from 'src/notification/firebase.module';
 
 @Module({
   
   controllers: [AlertController],
   providers: [alertService],
-  exports: [alertService] // agar kahin aur use karna ho to
+  exports: [alertService], // agar kahin aur use karna ho to
+    imports: [FirebaseAdminModule],
 })
 export class AlertModule {}
